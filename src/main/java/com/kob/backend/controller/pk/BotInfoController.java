7326f2c2803ab22c0d3e2c624f8242242e7f57a0
Hovey <2759651474@qq.com>
2022-10-05 18:37:43 +0800

    1

--- src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -3,7 +3,7 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController // 返回数据
 @RequestMapping("/pk/")
 public class BotInfoController {
     @RequestMapping("getbotinfo/")
